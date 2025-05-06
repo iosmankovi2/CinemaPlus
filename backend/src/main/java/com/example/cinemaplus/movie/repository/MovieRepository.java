7f2b47d4ca868701369ead7461abfaeb2ddbb7fc
Ilhana <igabela1@etf.unsa.ba>
2025-05-06 19:52:52 +0200

    Konflikti sređeni

--- backend/src/main/java/com/example/cinemaplus/movie/repository/MovieRepository.java
+++ backend/src/main/java/com/example/cinemaplus/movie/repository/MovieRepository.java
@@ -2,7 +2,6 @@ package com.example.cinemaplus.movie.repository;
 
 import com.example.cinemaplus.movie.model.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
 
 public interface MovieRepository extends JpaRepository<Movie, Long> {
--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,27 +1,14 @@
-import React, { useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react';
 import './Home.css';
 import popcornImage from '../assets/popcorn-cinema.jpg';
 
 const Home = () => {
-
   return (
     <div className="home-container">
-      {/* Navigacija */}
-      <header className="navbar">
-        <Link to="/" className="logo">
-          🎞️ <span><span className="cinema-red">Cinema</span><span className="plus-white">Plus</span></span>
-        </Link>
-        <nav className="nav-links">
-          <Link to="/sale" className="nav-link">Sale</Link> {/* Dodan link za Sale */}
-          <Link to="/login" className="nav-link">Login</Link>
-          <Link to="/register" className="nav-link">Register</Link>
-        </nav>
-      </header>
       <div className="home-background" style={{ backgroundImage: `url(${popcornImage})` }}>
         <div className="overlay">
           <div className="home-content">
-            {}
+            {/* Sadržaj za početnu stranicu ovdje */}
           </div>
         </div>
       </div>
